c5ec60ec8cdd9c737f85e506c883270698132063
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-04-18 21:22:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (19.97%) - LeetSync

--- Math/Easy/9-palindrome-number/palindrome-number.cpp
+++ Math/Easy/9-palindrome-number/palindrome-number.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0) return 0;
+        if(x < 0) return false;
         string str = to_string(x);
         int  l = 0;
         int r = str.size()-1;
         while(l<=r){
-            if(str[l]==str[r]){
-                l++;
-                r--;
-            }else{
+            if(str[l] != str[r]){
                 return false;
             }
+            l++;
+            r--;
         }
         return true;
     }
