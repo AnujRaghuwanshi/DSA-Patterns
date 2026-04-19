320571c57ad75e677dff9e0b103e8326066a99d5
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-04-19 14:16:00 +0530

    Update maximum-distance-between-a-pair-of-values.cpp

--- Two-Pointer/Medium/1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.cpp
+++ Two-Pointer/Medium/1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.cpp
@@ -11,12 +11,12 @@ public:
 
         while(i < n1 && j < n2){
             if(nums1[i] <= nums2[j]){
-                j++;
+                j++;                          //if nums1[i] <= nums2[j] increment j to find farthest element
             }
             else{
-                dist = max(dist, j-i-1);
+                dist = max(dist, j-i-1);     //calculate dist using (j-i-1) becz j++ will move it one pos ahead
                 i++;
-                if(i > j) j = i;
+                if(i > j) j = i;            //check whether i is greater then j or not
             }
         }
         return max(dist, j-i-1);
