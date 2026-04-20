da60139baa45d4931c7e23855bd395240e036b29
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-04-20 21:36:29 +0530

    Update two-furthest-houses-with-different-colors.cpp

--- Two-Pointer/Easy/2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
+++ Two-Pointer/Easy/2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.cpp
@@ -5,6 +5,7 @@ public:
         int dist = 0;
         
         int j = n - 1;
+        //Fix the pointer at 0 and Check from backward for different color
         while(j >= 0){
             if(colors[0] != colors[j]){
                 dist = max(dist,j-0);
@@ -15,6 +16,7 @@ public:
         }
 
         int i = 0;
+        //Fix the pointer at n-1 and Check from forward for different color
         while(i < n){
             if(colors[i] != colors[n-1]){
                 dist = max(dist,n-1-i);
