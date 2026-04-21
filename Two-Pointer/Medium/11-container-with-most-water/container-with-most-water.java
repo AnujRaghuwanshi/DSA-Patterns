cbfd453d0e8627f983cdf72314c4dbdbdb5815f8
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-04-21 22:28:48 +0530

    Time: 5 ms (83.11%) | Memory: 77.4 MB (52.23%) - LeetSync

---
+++ Two-Pointer/Medium/11-container-with-most-water/container-with-most-water.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int max_water = 0;
+
+        while(l < r){
+            int width = r - l;
+            int len = Math.min(height[l],height[r]);    //length will be minimum vertical line
+
+            max_water = Math.max(max_water,len * width);
+             //increment left if height of left pointer is less or right
+            if(height[l] < height[r]){
+                l++;
+            }else{
+                r--;
+            }           
+        }
+        
+        return max_water;
+    }
+}
