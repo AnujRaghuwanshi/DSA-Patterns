7f118b68d3484b1521d9f0a7054b1322e2465957
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-04-22 02:30:41 +0530

    Update search-in-rotated-sorted-array.cpp

--- Binary-search/Medium/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ Binary-search/Medium/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -52,3 +52,68 @@ int binarySearch(vector<int> nums, int low, int high, int target){
         return index == -1 ? index : (index + pivot) % n;         
     }
 };
+
+
+/*********************************************************************************/
+
+
+class Solution {
+public:
+
+int findpivotIndex(vector<int> nums){
+    int l = 0, r = nums.size()-1;
+    while(l<r){
+        int mid = (l+r)/2;
+        if(nums[mid]>nums[mid+1] && mid<r){
+              return mid+1;
+        }else if(mid>l && nums[mid]<nums[mid-1]){
+            return mid;
+        }
+        if(nums[mid]>=nums[l]){
+            l = mid+1;
+        }else{
+            r = mid-1;
+        }
+    }
+    return -1;
+}
+
+int binarySearch(vector<int> nums, int low, int high, int target){
+    int l = low, h = high-1;
+    while(l<=h){
+        int mid = (l+h)/2;
+        if(nums[mid]==target) return mid;
+        else if(nums[mid]<target){
+            l = mid+1;
+        }else{
+            h = mid-1;
+        }
+    }
+   return -1;
+}
+
+    int search(vector<int>& nums, int target) {
+
+        if(nums.size()==1 && nums[0]==target) return 0;
+
+        int pivot = findpivotIndex(nums);
+        int index = -1;
+
+        if(pivot == -1){
+            index = binarySearch(nums,0,nums.size(),target);
+            return index;
+        }
+
+        if(nums[pivot]==target) return pivot;
+
+        int before_pivotIndex = binarySearch(nums, 0 ,pivot,target);
+       
+        int after_pivotIndex = binarySearch(nums, pivot+1, nums.size(),target);
+      
+        if(before_pivotIndex == after_pivotIndex) return before_pivotIndex;
+        return  (before_pivotIndex > after_pivotIndex) ?  before_pivotIndex :after_pivotIndex;
+         
+
+    
+    }
+};
