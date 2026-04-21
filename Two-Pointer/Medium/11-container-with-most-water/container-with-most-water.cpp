8d36946a5d37f76486f3fe08af349f30e1b259c2
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-04-21 22:39:16 +0530

    Update container-with-most-water.cpp

--- Two-Pointer/Medium/11-container-with-most-water/container-with-most-water.cpp
+++ Two-Pointer/Medium/11-container-with-most-water/container-with-most-water.cpp
@@ -16,3 +16,26 @@ public:
         return max_water;
     }
 };
+
+//skip additional calculations using while loop
+/************************************************************/
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+        int max_water = 0;
+
+        while(l < r){
+            int width = r - l;
+            int len = min(height[l],height[r]);    //length will be minimum vertical line
+
+            max_water = max(max_water,len * width);
+            while(l < r && height[l] < len) l++;     //increment left if height of left pointer is less
+            while(l < r &&  height[r] < len) r--;    //increment right if height of right pointer is less
+        }
+        
+        return max_water;
+    }
+};
