2783b94670aa13d444874af292589abcde496c4c
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-04-21 22:35:58 +0530

    Update container-with-most-water.java

--- Two-Pointer/Medium/11-container-with-most-water/container-with-most-water.java
+++ Two-Pointer/Medium/11-container-with-most-water/container-with-most-water.java
@@ -20,3 +20,27 @@ class Solution {
         return max_water;
     }
 }
+
+
+//skip additional calculations using while loop
+/********************************************************************************/
+
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int max_water = 0;
+
+        while(l < r){
+            int width = r - l;
+            int len = Math.min(height[l],height[r]);    //length will be minimum vertical line
+
+            max_water = Math.max(max_water,len * width);
+             //increment left if height of left pointer is less or right
+            while(l < r && height[l] <= len) l++;
+            while(l < r && height[r] <= len) r--;          
+        }
+        
+        return max_water;
+    }
+}
