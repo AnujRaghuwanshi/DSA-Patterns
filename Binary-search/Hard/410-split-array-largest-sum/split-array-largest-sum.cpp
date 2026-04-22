43cb36ea162dd12d4f541dae93406221533b3498
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-04-22 23:55:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (39.84%) - LeetSync

---
+++ Binary-search/Hard/410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool isValid(vector<int>& nums, int k, int allowedSum){
+        int k_val = 1, maxSum = 0;
+        for(int val : nums){
+            if(val > allowedSum) return false;
+            if(maxSum + val <= allowedSum){   //add in first subarray until it exceeds allowedSum
+                maxSum += val;
+            }else{
+                //start new subarray and increment k_val
+                k_val++;
+                maxSum = val;
+            }
+        }
+        return (k_val <= k) ? true : false;  //if k_val <= k return true else false
+    }
+    int splitArray(vector<int>& nums, int k) {
+        //task is to fild minimum possible largest sum
+        int sum = 0;
+        for(int val : nums){     //O(n)
+            sum += val;
+        }
+
+        int st = 0;         //min possible = 0
+        int end = sum;      //max possible = sum of array
+
+        //answer lies between 0 to sum
+        int ans = -1;
+
+        //T.C. O(nlog(sum))
+        while(st <= end){      //O(log(sum))
+            int mid = st + (end-st)/2;
+            
+            if(isValid(nums,k,mid)){    //O(n)
+            //if mid is valid search in left because we need to find minimum largest sum
+                ans = mid;
+                end = mid-1;
+            }else{
+            //if mid is invalid search in right because all elemnts in left will be invalid
+                st = mid+1;
+            }
+        }
+        return ans;
+    }
+};
