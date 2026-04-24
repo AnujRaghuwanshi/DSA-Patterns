099c7a0d196c1a24732649a06eecc97accc5e1aa
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-04-25 00:32:39 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (65.27%) - LeetSync

--- HashMap/Medium/3019-furthest-point-from-origin/furthest-point-from-origin.cpp
+++ HashMap/Medium/3019-furthest-point-from-origin/furthest-point-from-origin.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int furthestDistanceFromOrigin(string moves) {
-        int countL = 0, countR = 0,space = 0;
+        int count = 0, space = 0;
         for(const auto& ch : moves){
-            if(ch == 'L') countL++;
-            else if(ch == 'R') countR++;
-            else space++;
+            if(ch == '_') space++;
+            else if(ch == 'L') count--;
+            else count++;
         }
-        if(countL == countR) return space;
-        else if(countL > countR) return abs(countL-countR+space);
-        return abs(countR-countL+space);
+        if(count == 0) return space;
+        else if(count < 0) return abs(count-space);
+        return count+space;
     }
 };
