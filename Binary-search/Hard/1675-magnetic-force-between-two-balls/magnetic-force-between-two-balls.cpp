9f9ee0d96fb337535233887515b2e76d2bd6a11f
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-05-08 21:50:27 +0530

    Improve isPossible and maxDistance functions
    Refactor isPossible and maxDistance functions for clarity and performance.

--- Binary-search/Hard/1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
+++ Binary-search/Hard/1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isPossible(vector<int>& arr,int n, int m, int force){
+    bool isPossible(vector<int>& arr,int n, int m, int force){  //O(N)
         int lastbasket = arr[0], balls = 1;
         m--;
         for(int i = 1; i < n; i++){
@@ -14,7 +14,7 @@ public:
     }
 
     int maxDistance(vector<int>& position, int m) {
-        sort(position.begin(),position.end());
+        sort(position.begin(),position.end());   //O(NlogN)
         int n = position.size();
 
         int st = 1;
@@ -22,7 +22,7 @@ public:
 
         int ans = -1;
 
-        while(st <= end){
+        while(st <= end){     //O(log(Range)*N)
             int mid = st + (end-st)/2;
             if(isPossible(position,n,m,mid)){
                 ans = mid;
