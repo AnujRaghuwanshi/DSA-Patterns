c6fe3e7d897865f0ed3d9343a1c05a696e7418c2
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-05-08 21:45:27 +0530

    Time: 45 ms (75.19%) | Memory: 61.7 MB (36.81%) - LeetSync

---
+++ Binary-search/Hard/1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& arr,int n, int m, int force){
+        int lastbasket = arr[0], balls = 1;
+        m--;
+        for(int i = 1; i < n; i++){
+            if((arr[i] - lastbasket) >= force){
+                lastbasket = arr[i];
+                m--;
+            }
+            if(m == 0) return true;
+        }
+        return m == 0;
+    }
+
+    int maxDistance(vector<int>& position, int m) {
+        sort(position.begin(),position.end());
+        int n = position.size();
+
+        int st = 1;
+        int end = position[n-1] - position[0];
+
+        int ans = -1;
+
+        while(st <= end){
+            int mid = st + (end-st)/2;
+            if(isPossible(position,n,m,mid)){
+                ans = mid;
+                st = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+};
