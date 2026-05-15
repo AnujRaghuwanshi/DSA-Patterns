866024122134cecb4c810021204b4c609a73f22f
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-05-15 11:03:33 +0530

    Add findMin function to locate minimum element
    Implement binary search to find the minimum in rotated sorted array.

--- Binary-search/Medium/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ Binary-search/Medium/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -20,3 +20,28 @@ public:
         return ans;
     }
 };
+
+/*************************************************************************************/
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1 || (nums[0] < nums[n-1])) return nums[0];
+
+        int st = 0;
+        int end = n-1;
+        int ans = INT_MAX;
+        while(st <= end){
+            int mid = st + (end-st)/2;
+            if(nums[st] <= nums[mid]){
+                ans = min(ans,nums[st]);
+                st = mid+1;
+            }else{
+                ans = min(ans,nums[mid]);
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+};
