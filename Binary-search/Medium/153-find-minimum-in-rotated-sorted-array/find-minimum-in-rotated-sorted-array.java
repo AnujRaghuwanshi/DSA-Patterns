ccd31c5ba46a85a549cf47cf5d9fad92b52af1c4
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-05-15 11:08:16 +0530

    Implement findMin method for rotated sorted array

--- Binary-search/Medium/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ Binary-search/Medium/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -20,3 +20,28 @@ class Solution {
         return ans;
     }
 }
+
+/*************************************************************************************/
+
+class Solution {
+    public int findMin(int[] nums) {
+        int n = nums.length;
+        if(n == 1 || (nums[0] < nums[n-1])) return nums[0];
+
+        int st = 0;
+        int end = n-1;
+        int ans = Integer.MAX_VALUE;
+
+        while(st <= end){
+            int mid = st + (end-st)/2;
+            if(nums[st] <= nums[mid]){
+                ans = Math.min(ans,nums[st]);
+                st = mid+1;
+            }else{
+                ans = Math.min(ans,nums[mid]);
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+}
