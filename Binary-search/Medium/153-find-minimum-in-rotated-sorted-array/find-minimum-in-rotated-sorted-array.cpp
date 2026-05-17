3d8c18b2394bcc631ebf61afb06a1ec97b6faaa0
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-05-17 09:51:40 +0530

    Refactor findMin to use binary search for minimum

--- Binary-search/Medium/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ Binary-search/Medium/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -25,23 +25,26 @@ public:
 
 class Solution {
 public:
+    int Solve(vector<int>& nums, int n){
+        int st = 0;
+        int end = n-1;
+        while(st <= end){
+            int mid = st + (end-st)/2;
+
+            if(nums[mid] > nums[mid+1]) return mid;
+
+            else if(nums[mid] < nums[mid-1]) return mid-1;
+
+            else if(nums[st] < nums[mid]) st = mid+1;
+
+            else end = mid-1;
+        }
+        return -1;
+    }
     int findMin(vector<int>& nums) {
         int n = nums.size();
         if(n == 1 || (nums[0] < nums[n-1])) return nums[0];
-
-        int st = 0;
-        int end = n-1;
-        int ans = INT_MAX;
-        while(st <= end){
-            int mid = st + (end-st)/2;
-            if(nums[st] <= nums[mid]){
-                ans = min(ans,nums[st]);
-                st = mid+1;
-            }else{
-                ans = min(ans,nums[mid]);
-                end = mid-1;
-            }
-        }
-        return ans;
+        int idx = Solve(nums,n);   //find index of maximum element
+        return nums[idx+1 %  n];   //minimum element will be on idx of maximum element + 1
     }
 };
