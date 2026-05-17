65cd5b752d326d40a9c43a57ff174eb85ef59e66
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-05-17 23:26:32 +0530

    Time: 3 ms (15.18%) | Memory: 7.9 MB (40.52%) - LeetSync

--- Binary-search/Medium/278-first-bad-version/first-bad-version.cpp
+++ Binary-search/Medium/278-first-bad-version/first-bad-version.cpp
@@ -5,16 +5,14 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int st = 1;
-        int ans = -1;
         while(st <= n){
             int mid = st + (n - st)/2;
             if(isBadVersion(mid)){
-                ans = mid;
                 n = mid-1;
             }else{
                 st = mid+1;
             }
         }
-        return ans;
+        return st;
     }
 };
