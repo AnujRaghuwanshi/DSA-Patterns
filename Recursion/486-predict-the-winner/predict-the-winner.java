29f1bf3fff5e3a8b88184b190067a5ac54e37830
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-08-02 00:13:00 +0530

    Time: 70 ms (16.33%) | Memory: 42.8 MB (57.54%) - LeetSync

--- Recursion/486-predict-the-winner/predict-the-winner.java
+++ Recursion/486-predict-the-winner/predict-the-winner.java
@@ -1,28 +1,15 @@
 class Solution {
-    int[][] dp = new int[22][22];
     int Solve(int i, int j, int[] nums){
         if(i > j) return 0;
         if(i == j) return nums[i];
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int take_i = nums[i] + Math.min(Solve(i+2,j,nums),Solve(i+1,j-1,nums));
-        int take_j = nums[j] + Math.min(Solve(i,j-2,nums),Solve(i+1,j-1,nums));
-
-        return dp[i][j] = Math.max(take_i,take_j);
+        int take_i = nums[i] - Solve(i+1,j,nums);
+        int take_j = nums[j] - Solve(i,j-1,nums);
+        return Math.max(take_i,take_j);
     }
     public boolean predictTheWinner(int[] nums) {
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
-        }
         int n = nums.length;
-        int total = 0;
-        for(int val : nums){
-            total += val;
-        }
         int p1_score = Solve(0,n-1,nums);
-        int p2_score = total - p1_score;
-        if(p2_score > p1_score) return false;
-        return true;
-
+        if(p1_score >= 0) return true;
+        return false;
     }
 }
