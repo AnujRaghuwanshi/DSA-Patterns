3fac19efc95247e85877773c66d7250ac66b00ca
AnujRaghuwanshi <124479521+AnujRaghuwanshi@users.noreply.github.com>
2026-08-02 00:24:26 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (52.91%) - LeetSync

--- Recursion/486-predict-the-winner/predict-the-winner.cpp
+++ Recursion/486-predict-the-winner/predict-the-winner.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
+   vector<vector<int>> dp{22, vector<int>(22, -1)};
+
     int Solve(int i, int j, vector<int>& nums){
         if(i > j) return 0;
         if(i == j) return nums[i];
+        if(dp[i][j] != -1) return dp[i][j];
         int take_i = nums[i] - Solve(i+1,j,nums);
         int take_j = nums[j] - Solve(i,j-1,nums);
-        return max(take_i,take_j);
+        return dp[i][j] = max(take_i,take_j);
     }
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
